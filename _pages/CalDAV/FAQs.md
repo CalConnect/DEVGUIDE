23f2df1d2932455667be592962d07d330a565710
Georgiana Nistor <georgiana.nistor@1and1.ro>
2018-03-16 19:54:16 +0200

    Initial layout for FAQs pages

---
+++ _pages/CalDAV/FAQs.md
@@ -0,0 +1,31 @@
+---
+layout: toc-type
+title: FAQs
+parent: "/CalDAV"
+mainParent: "CalDAV"
+parents: "CalDAV:/CalDAV"
+order: 6
+---
+<div class="data-expander">
+    <a href="#">
+        Expand
+        <i class="material-icons">add</i>
+    </a>
+    <div class="data-expander-child">
+    dsvsvsvsvs
+
+    </div>
+</div>
+
+<div class="data-expander">
+    <a href="#">
+        Expand
+        <i class="material-icons">add</i>
+    </a>
+    <div class="data-expander-child">
+    dsvsvsvsvs<br>
+    dsvsvsvsvs<br>
+    dsvsvsvsvs<br>
+    dsvsvsvsvs<br>
+    </div>
+</div>
--- js/init.js
+++ js/init.js
@@ -4,5 +4,18 @@
     $('.button-collapse').sideNav();
     $('.carousel').carousel();
 
+    $( ".data-expander a" ).click(function(evt) {
+        evt.preventDefault();
+        var expandParent = $(this).parent();
+        var expandChild = expandParent.find('.data-expander-child');
+        var expandChildHeight = expandChild.height();
+
+        $( ".data-expander" ).each(function( index ) {
+            $(this).height(28);
+        });
+        
+        expandParent.height(expandChildHeight + 34);
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
